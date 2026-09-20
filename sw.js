7bccd70b12b13e6d9c7161ca0654f66197637df4
Milenko Raic <milenko.mika.raic@gmail.com>
2026-09-21 01:30:53 +0200

    Fix GitHub Pages footer cache mismatch

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_VERSION = 'ark-os-v42-core-7';
+const CACHE_VERSION = 'ark-os-v43-footer-1';
 const CORE_ASSETS = [
   './',
   './index.html',
-  './index.css?v=42-core-7',
+  './index.css?v=43-footer-1',
   './manifest.webmanifest',
   './icons/icon-192.png',
   './icons/icon-512.png',
